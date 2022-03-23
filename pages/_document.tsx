fed43c8dd241f6e243f86eae4a371efd606100f7
seoyoung-dev <seoyoung.dev@gmail.com>
2022-03-23 22:45:53 +0900

    #5 Build ImgCard component layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Noto+Serif+KR:wght@200;600;700&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Noto+Serif+KR:wght@200;600;700&family=Roboto:wght@300;500;700&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
