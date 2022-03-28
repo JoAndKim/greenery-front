6491e9599163f0d54d11b345a7bf7538cc7114eb
seoyoung-dev <seoyoung.dev@gmail.com>
2022-03-28 22:50:15 +0900

    #4 Add head information using next/head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>초록친구</title>
+                <meta
+                    property="og:title"
+                    content="초록친구 바로가기"
+                    key="greenery_title"
+                />
+                <meta
+                    property="og:description"
+                    content="반려식물을 죽이지 않고 '잘' 기르고 싶은 사람들을 위한 서비스"
+                    key="greenery_description"
+                />
+                <meta property="og:image" content="#" key="greenery_image" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
