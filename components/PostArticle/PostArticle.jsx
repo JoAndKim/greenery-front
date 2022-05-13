898edc5fff908909561fae912c9ea34a7cdce103
seoyoung-dev <seoyoung.dev@gmail.com>
2022-05-13 23:22:21 +0900

    #12 Update Article, PostArticle function parameter

--- components/PostArticle/PostArticle.jsx
+++ components/PostArticle/PostArticle.jsx
@@ -5,35 +5,11 @@ import {
     DetailedInfo,
     Like,
     ContentBox,
-    Content,
+    Content
 } from "./PostArticle.style";
-
-// {
-//   "id": 1,
-//   "user": {
-//       "id": 3,
-//       "username": "dongkyu",
-//       "profileImageUrl": "http://s3.aws.com/test.jpg"
-//   },
-//   "title": "좋아하는 식물들",
-//   "likes": 56,
-//   "likeStatus": true, // 좋아요 상태 (참고: 게스트일 경우 false로 통일)
-//   "postContents": [
-//       {
-//           "postImageUrl": "http://s3.aws.com/hello.jpg",
-//           "content": "금방 시들어 버리는 꽃이 아쉬우셨나요? 그렇다면 발코니를 FEJKA 페이카 인조식물과 조화로 채워 초록이 무성한 미니 정원을 만들어보세요."
-//       },
-//       {
-//           "postImageUrl": "http://s3.aws.com/hlhlhlhl.jpg",
-//           "content": "양치식물부터 뾰족한 가시가 달린 다육식물, 잎이 달린 다양한 식물까지 가정용 화초를 두면 집안 분위기가 훨씬 아늑해져요"
-//       },
-//   ],
-// "regDate": "2022-11-27",
-// }
 
 export default function PostArticle({
     postId,
-    user,
     title,
     likes,
     likeStatus,
@@ -41,7 +17,7 @@ export default function PostArticle({
     regDate,
     profileImgUrl,
     username,
-    userId,
+    userId
 }) {
     return (
         <ArticleWrapper>
@@ -60,15 +36,16 @@ export default function PostArticle({
                 </DetailedInfo>
             </PostInfo>
             <ContentBox>
-                {postContents.map((card, index) => (
-                    <Content key={`content-${index}`}>
-                        <img
-                            src={`${card.postImageUrl}`}
-                            alt="포스트 이미지"
-                        ></img>
-                        <p>{card.content}</p>
-                    </Content>
-                ))}
+                {postContents &&
+                    postContents.map((card, index) => (
+                        <Content key={`content-${index}`}>
+                            <img
+                                src={`${card.postImageUrl}`}
+                                alt="포스트 이미지"
+                            ></img>
+                            <p>{card.content}</p>
+                        </Content>
+                    ))}
             </ContentBox>
         </ArticleWrapper>
     );
--- container/Article/Article.jsx
+++ container/Article/Article.jsx
@@ -1,65 +1,66 @@
 import { Main, PostArticleWrapper } from "./Article.style";
-import { Header, SideBar, PostArticle } from "../../components/index";
-
-const dummy = {
-    id: 1,
-    user: {
-        id: 3,
-        username: "dongkyu",
-        profileImageUrl: "img/community.png",
-    },
-    title: "좋아하는 식물들",
-    likes: 56,
-    likeStatus: true, // 좋아요 상태 (참고: 게스트일 경우 false로 통일)
-    postContents: [
-        {
-            postImageUrl: "img/community.png",
-            content:
-                "금방 시들어 버리는 꽃이 아쉬우셨나요? 그렇다면 발코니를 FEJKA 페이카 인조식물과 조화로 채워 초록이 무성한 미니 정원을 만들어보세요.",
-        },
-        {
-            postImageUrl: "img/community.png",
-            content:
-                "양치식물부터 뾰족한 가시가 달린 다육식물, 잎이 달린 다양한 식물까지 가정용 화초를 두면 집안 분위기가 훨씬 아늑해져요",
-        },
-    ],
-    regDate: "2022-11-27",
-};
+import axios from "axios";
+import { useRouter } from "next/router";
+import {
+    Header,
+    SideBar,
+    PostArticle,
+    Comment,
+    Footer
+} from "../../components/index";
+import { useState, useEffect } from "react";
 
 export default function Article() {
+    const [postData, setPostData] = useState({});
+    const router = useRouter();
+    const postId = router.query.id;
+
+    useEffect(async () => {
+        if (!router.isReady) return;
+        const postId = await router.query.id;
+        const accessToken = await localStorage.getItem("accessToken");
+        const response = await axios.get(`/api/posts/${postId}`);
+        const { data } = response;
+        console.log(data);
+        setPostData(data);
+    }, [router.isReady]);
+
+    const { id, title, likes, likeStatus, postContents, regDate, user } =
+        postData;
+
     return (
         <Main>
             <Header />
 
             <PostArticleWrapper>
-                <PostArticle
-                    postId={dummy.id}
-                    user={dummy.user}
-                    title={dummy.title}
-                    likes={dummy.likes}
-                    likeStatus={dummy.likeStatus}
-                    postContents={dummy.postContents}
-                    regDate={dummy.regDate}
-                    profileImgUrl={dummy.user.profileImageUrl}
-                    username={dummy.user.username}
-                    userId={dummy.user.id}
-                />
-                {/* 
-                {article.author && (
-                    <SideBar
-                        likeHandler={handleLikeClick}
-                        commentHandler={handleCommentClick}
-                        trashHandler={handleTrashClick}
-                        postId={postId}
-                        updateHandler={handleUpdateClick}
-                        userId={userProfile.id}
-                        PostUserId={article.author}
-                        liked={liked}
+                {postData.user && (
+                    <PostArticle
+                        postId={id}
+                        title={title}
+                        likes={likes}
+                        likeStatus={likeStatus}
+                        postContents={postContents}
+                        regDate={regDate}
+                        profileImgUrl={user.profileImageUrl}
+                        username={user.nickname}
+                        userId={user.userId}
                     />
-            */}
-                {/* <div ref={commentRef} />
-                <Comment /> */}
+                )}
+
+                <SideBar
+                    likeHandler
+                    commentHandler
+                    trashHandler
+                    postId
+                    updateHandler
+                    userId
+                    PostUserId
+                    liked
+                />
+
+                <Comment />
             </PostArticleWrapper>
+            <Footer />
         </Main>
     );
 }
