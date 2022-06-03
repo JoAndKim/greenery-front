ab7897a4571e524a3be80cf54d38cc9aa365a107
seoyoung-dev <seoyoung.dev@gmail.com>
2022-06-03 17:26:18 +0900

    #14 Set states again when refresh page

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
-import { loginState } from "../../state";
+import { loginState, userInfoState } from "../../state";
 
 export default function Layout({ children }) {
     const setLoginState = useSetRecoilState(loginState);
+    const setUserInfoState = useSetRecoilState(userInfoState);
     const onReload = () => {
         const userInfo = localStorage.getItem("userInfo");
-        console.log("set data on global state");
         if (userInfo) {
             setLoginState(true);
+            setUserInfoState(userInfo);
         }
     };
 
