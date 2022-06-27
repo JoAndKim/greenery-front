4f9d3585a538063295a64c204d066fbaa5f32293
seoyoung-dev <seoyoung.dev@gmail.com>
2022-06-27 18:06:04 +0900

    Update _app.js and _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,21 +6,6 @@ import { Layout } from "../components/index";
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Head>
-                <link rel="shortcut icon" href="/favicon.svg" />
-                <title>초록친구</title>
-                <meta
-                    property="og:title"
-                    content="초록친구 바로가기"
-                    key="greenery_title"
-                />
-                <meta
-                    property="og:description"
-                    content="반려식물을 죽이지 않고 '잘' 기르고 싶은 사람들을 위한 서비스"
-                    key="greenery_description"
-                />
-                <meta property="og:image" content="#" key="greenery_image" />
-            </Head>
             <RecoilRoot>
                 <Layout>
                     <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, {
     Head,
     Main,
     NextScript,
-    DocumentContext,
+    DocumentContext
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
+                        sheet.collectStyles(<App {...props} />)
                 });
 
             const initialProps = await Document.getInitialProps(ctx);
@@ -26,7 +26,7 @@ class MyDocument extends Document {
                         {initialProps.styles}
                         {sheet.getStyleElement()}
                     </>
-                ),
+                )
             };
         } finally {
             sheet.seal();
@@ -40,6 +40,23 @@ class MyDocument extends Document {
                     <link
                         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Noto+Serif+KR:wght@200;600;700&family=Roboto:wght@300;500;700&display=swap"
                         rel="stylesheet"
+                    />
+                    <link rel="shortcut icon" href="/favicon.svg" />
+                    <title>초록친구</title>
+                    <meta
+                        property="og:title"
+                        content="초록친구 바로가기"
+                        key="greenery_title"
+                    />
+                    <meta
+                        property="og:description"
+                        content="반려식물을 죽이지 않고 '잘' 기르고 싶은 사람들을 위한 서비스"
+                        key="greenery_description"
+                    />
+                    <meta
+                        property="og:image"
+                        content="#"
+                        key="greenery_image"
                     />
                 </Head>
                 <body>
