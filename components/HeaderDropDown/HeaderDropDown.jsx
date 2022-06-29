e795545b890ec6e9bf09cdf66f34a79df32b8b0c
seoyoung-dev <seoyoung.dev@gmail.com>
2022-06-29 23:30:51 +0900

    Add logout function

--- components/HeaderDropDown/HeaderDropDown.jsx
+++ components/HeaderDropDown/HeaderDropDown.jsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
+import { loginState, userInfoState } from "../../state";
+import { useResetRecoilState } from "recoil";
 import { ContentWrapper, StyledLink } from "./HeaderDropDown.style";
 
 export default function HeaderDropDown({ loginState }) {
+    const resetUserState = useResetRecoilState(userInfoState);
+    const resetLoginState = useResetRecoilState(loginState);
+
     const beforeLoginOptions = [
         {
             title: "로그인",
@@ -22,6 +27,12 @@ export default function HeaderDropDown({ loginState }) {
             to: "/"
         }
     ];
+
+    const handleLogout = () => {
+        resetUserState();
+        resetLoginState();
+        localStorage.clear();
+    };
     const linkOption = loginState ? afterLoginOptions : beforeLoginOptions;
 
     return (
