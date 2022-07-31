392bb7443a39b83db9cb83385944495dee5dd3df
seoyoung-dev <seoyoung.dev@gmail.com>
2022-07-31 23:47:18 +0900

    Fix typo

--- container/MyPage/MyPage.jsx
+++ container/MyPage/MyPage.jsx
@@ -40,7 +40,7 @@ export default function MyPage() {
 
     return (
         <>
-            <Header></Header>
+            <Header />
             <ProfileWrapper>
                 <ProfileImg>
                     <img
